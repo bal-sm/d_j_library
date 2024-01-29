016ec24408c99e10372fdaa9432cc0b029f08bd6
Mahmuda <bal.mahmuda@gmail.com>
2024-01-29 13:21:07 +0700

    Refactored `details` field to its own `Model` class on `dcustom` and applied to every model that has `details` field before
    wait it's not cleanly migrate

---
+++ dcustom/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class JSONDetailsMixin(models.Model):
+    """
+    A mixin for models that have `JSONField` `details`.
+    """
+
+    details = models.JSONField(
+        null=True,
+        blank=True,
+        verbose_name=_("detil-detil lainnya"),
+    )
+
+    class Meta:
+        abstract = True
