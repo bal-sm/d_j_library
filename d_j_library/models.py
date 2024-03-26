2bd399c6ab52150f9b2542fa553d68e8961a58d7
Mahmuda <bal.mahmuda@gmail.com>
2024-03-26 13:49:38 +0700

    Removed `DJangoOrderMixin`

--- d_j_library/models.py
+++ d_j_library/models.py
@@ -2,17 +2,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class DJangoOrderMixin(models.Model):
-    d_j_order = models.PositiveBigIntegerField(
-        editable=False,
-        verbose_name=_("ke-"),
-    )
-
-    class Meta:
-        abstract = True
-        ordering = ["d_j_order"]
-
-
 class JSONDetailsMixin(models.Model):
     """
     A mixin for models that have `JSONField` `details`.
